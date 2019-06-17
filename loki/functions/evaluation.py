1d32e7aa000df85e1643c75cb4c6a0805b5a35a3
TensorDuck <jchen978@gmail.com>
2019-06-17 16:51:21 -0700

    Moved getting statistics to separate func
    The option should exist for the user to get the statistics without 
    having to print the statistics involving the confusion matrix.

--- loki/functions/evaluation.py
+++ loki/functions/evaluation.py
@@ -1,15 +1,34 @@
 """Useful functions for evaluating a model's performance"""
 import sklearn.metrics as skmet
 
-def print_confusion_matrix(true, predicted):
-    """Print out the confusion matrix"""
+def get_confusion_matrix(actual, predicted):
+    """Get the confusion matrix and statistics
 
+    Arguments:
+    ----------
+    actual -- np.ndarray:
+        The atual classes,
+    predicted -- np.ndarray:
+        The inferred classes
+
+    Return:
+    -------
+    """
     cm = skmet.confusion_matrix(test_values, results)
 
     accuracy = (cm[0,0] + cm[1,1]) / len(test_values)
 
     precision = cm[1,1] / (cm[0,1] + cm[1,1]) # true positives over false positives and true positives
     recall = cm[1,1] / (cm[1,0] + cm[1,1]) # True positives over false negatives and true positives
+
+    results = {"cm":cm, "accuracy":accuracy, "precision":precision, "recall":recall}
+
+    return results
+
+def print_confusion_matrix(actual, predicted):
+    """Print out the confusion matrix"""
+
+    results = get_confusion_matrix(actual, predicted)
 
     print(cm)
 
