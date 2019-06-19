5bb1010ff295fb03b8312480732db1e8a8a5459d
TensorDuck <jchen978@gmail.com>
2019-06-18 20:00:00 -0700

    Implemented function to get sound embeddings
    The function get_embeddings() is meant to isolate the VGGish network 
    from the rest of the code. This takes a raw sound input and its sampling 
    rate, and then outputs the embeddings.

---
+++ loki/models/neural_networks.py
@@ -0,0 +1,36 @@
+"""This package contains methods for using neural networks"""
+import tensorflow as tf
+from vggish_tensorflow import CreateVGGishNetwork, EmbeddingsFromVGGish
+import numpy as np
+
+def get_embeddings(x, sr):
+    """Get the sound embeddings from vgg-ish
+
+    Use the pre-trained vggish network from TensorFlow in order to
+    extract embeddings from an audio clip. The vggish network does all
+    the preprocesisng necessary on a raw audio input.
+
+    x can be arbitrary length, but the VGGish network was trained on
+    0.96 second clips. As a result, the dimensions of the output is
+    going to be 128 x M, where M = floor(time(x) / 0.96).
+    i.e. a 10 second clip produces a 128 x 10 output.
+
+    Arguments:
+    ----------
+    x -- numpy.ndarray:
+        The trace of the sound wave (mono-channel)
+    sr -- int:
+        The sampling rate for the audio clip in Hz.
+    """
+    checkpoint_path = 'vggish_tensorflow/vggish_model.ckpt'
+
+    tf.reset_default_graph()
+    sess = tf.Session()
+
+
+    vgg = CreateVGGishNetwork(sess, checkpoint_path)
+    resdict = EmbeddingsFromVGGish(sess, vgg, x, sr)
+
+    sess.close()
+
+    return resdict['embedding']
