d50ed7685507354e15668afcca6435d24ce24942
TensorDuck <jchen978@gmail.com>
2019-06-20 11:31:08 -0700

    Added env variable to build for vggish ckpt

--- build/add_path.sh
+++ build/add_path.sh
@@ -2,3 +2,5 @@ cd ..
 CURRENT=`pwd`
 cd build
 export PYTHONPATH="$CURRENT:$PYTHONPATH"
+#export SOUNDEMBEDDINGS="$CURRENT/loki/models/vggish_tensorflow/vggish_model.ckpt"
+export SOUNDEMBEDDINGS="$CURRENT/build/vggish_model.ckpt"
