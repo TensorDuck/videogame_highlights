012e8069c1f2d624f2b99fb5345dd33bf4ecfff6
TensorDuck <jchen978@gmail.com>
2019-06-29 18:55:02 -0700

    Implemented `nn` option to train_classifier()
    Added a `nn` option to the loki.functions.helper.train_classifier() 
    function. Added the keyword arguments for the NeuralNetworkClassifier, 
    documentation indicating those kwargs are only used for training a 
    neural network, and prints an error if an invalid classifier is 
    specified.

--- loki/functions/helper.py
+++ loki/functions/helper.py
@@ -40,7 +40,7 @@ def load_clips_from_dir(target_dir=None):
 
     return clips
 
-def train_classifier(train_clips, train_targets, test_clips=None, test_targets=None, classifier="nn"):
+def train_classifier(train_clips, train_targets, test_clips=None, test_targets=None, classifier="nn", n_epochs=100, batch_size=None, class_weights=None):
     """Get a trained classifier for audio data
 
     Return a trained classifier. If test_clips and test_targets is
@@ -67,13 +67,27 @@ def train_classifier(train_clips, train_targets, test_clips=None, test_targets=N
         Type of classifier to train. `nn` returns a
         loki.NeuralNetworkClassifier while 'volume' returns a
         loki.VolumeClassifier.
+    n_epochs -- int -- default=100:
+        Number of training epochs to run. Only used for classifier=`nn`.
+    batch_size -- int -- default=all:
+        Batch size of each training epoch. Default is all training
+        data at each epoch. Only used for classifier=`nn`.
+    class_weights -- np.ndarray -- default=None:
+        Relative weight of each class. This weight affects the
+        probability of picking each class when selecting the batch. Only
+        used for classifier=`nn`.
 
     Return:
     -------
     classifier -- loki.VolumeClassifier:
         A trained classifier.
     """
-    classifer = _train_volume_classifier(train_clips, train_targets)
+    if classifier == "volume":
+        classifer = _train_volume_classifier(train_clips, train_targets)
+    elif classifier == "nn":
+        classifier = _train_nn_classifier(train_clips, train_targets)
+    else:
+        print("Invalid Classifier Specified. Keyword wargument classifier must be either 'volume' or 'nn'.")
 
     if test_clips is not None and test_targets is not None:
         #compute validation of test_clips is given
