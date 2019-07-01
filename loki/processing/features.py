ad1bb4621c65e6a7ca9fb94bf0fcd3b38fd6170d
TensorDuck <jchen978@gmail.com>
2019-06-30 21:34:26 -0700

    Removed obsolete if-statement.
    Check already happens in the VideoClips() class. No need to check again 
    here.

--- loki/processing/features.py
+++ loki/processing/features.py
@@ -25,11 +25,8 @@ def compute_decibels(data, freq=44100):
     """
     decibels = []
 
-    if data.audios is None:
-        all_audio = data.compute_audio_waveform(freq=freq)
-    else:
-        all_audio = data.audios
-
+    all_audio = data.compute_audio_waveform(freq=freq)
+    
     for binaural in all_audio:
         power = binaural ** 2 # square for the power
         #sum up binaural audio channel
