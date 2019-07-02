beb3ad9ca713f6ab0cc76449d6beb7f1b913fb65
TensorDuck <jchen978@gmail.com>
2019-07-01 20:55:53 -0700

    Added wget statement to download ckpt file

--- build/add_path.sh
+++ build/add_path.sh
@@ -1,6 +1,14 @@
+#set the necessary path variables
 cd ..
 CURRENT=`pwd`
 cd build
 export PYTHONPATH="$CURRENT:$PYTHONPATH"
-#export SOUNDEMBEDDINGS="$CURRENT/loki/models/vggish_tensorflow/vggish_model.ckpt"
 export SOUNDEMBEDDINGS="$CURRENT/build/vggish_model.ckpt"
+
+#download the vggish model checkpoint file if it does not already exist
+if test -f "vggish_model.ckpt"; then
+echo vggish_model.ckpt is already downloaded.
+else
+echo downloading vggish_model.ckpt
+wget https://storage.googleapis.com/audioset/vggish_model.ckpt
+fi
